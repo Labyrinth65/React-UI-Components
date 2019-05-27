8dc787e79438cce738d732bf90c5587a55cb5b5d
Ang Xu <angxu1234@gmail.com>
2019-05-27 17:28:49 -0500

    HeaderTitle and Content started

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+function HeaderTitle() {
+	return <div>Lambda School</div>;
+}
+
+export default HeaderTitle;
