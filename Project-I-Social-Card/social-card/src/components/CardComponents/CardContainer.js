5f9f033480e4e50689f9e9d9ae3b20714fb727b2
Ang Xu <angxu1234@gmail.com>
2019-05-28 08:54:43 -0500

    CardContainer link added

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -3,17 +3,40 @@ import "./Card.css";
 import CardBanner from "./CardBanner";
 import CardContent from "./CardContent";
 
-function CardContainer() {
-	return (
-		<div className="cardContainer">
-			<div className="banner">
-				<CardBanner />
+// function CardContainer() {
+// 	return (
+// <div className="cardContainer">
+// 	<div className="banner">
+// 		<CardBanner />
+// 	</div>
+// 	<div className="cardText">
+// 		<CardContent />
+// 	</div>
+// </div>
+// 	);
+// }
+
+class CardContainer extends React.Component {
+	handleClick() {
+		window.location.href = "https://www.reactjs.org";
+	}
+
+	render() {
+		return (
+			<div
+				className="cardContainer"
+				onClick={this.handleClick}
+				style={{ cursor: "pointer" }}
+			>
+				<div className="banner">
+					<CardBanner />
+				</div>
+				<div className="cardText">
+					<CardContent />
+				</div>
 			</div>
-			<div className="cardText">
-				<CardContent />
-			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default CardContainer;
