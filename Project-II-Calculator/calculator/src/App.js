dbf764005b5186bc321d54eeb9c1dc09b9a1f658
Ang Xu <angxu1234@gmail.com>
2019-05-28 15:24:23 -0500

    Added some JS to create buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -6,6 +6,8 @@ import MathButton from "./components/ButtonComponents/MathButton";
 import NumberButton from "./components/ButtonComponents/NumberButton";
 
 const App = () => {
+	const mathSymbols = ["÷", "×", "−", "+", "="];
+	const numbers = [[7, 8, 9], [4, 5, 6], [1, 2, 3]];
 	return (
 		<div className="calculator">
 			<div className="calculatorTop">
@@ -16,33 +18,55 @@ const App = () => {
 					<ActionButton buttonStyle="clear" text="clear" />
 					<div className="numbersOneToNine">
 						<div className="firstRow">
-							<NumberButton buttonStyle="number" text="7" />
-							<NumberButton buttonStyle="number" text="8" />
-							<NumberButton buttonStyle="number" text="9" />
+							{numbers[0].map(el => (
+								<NumberButton buttonStyle="number" text={el} />
+							))}
 						</div>
 						<div className="secondRow">
-							<NumberButton buttonStyle="number" text="4" />
-							<NumberButton buttonStyle="number" text="5" />
-							<NumberButton buttonStyle="number" text="6" />
+							{numbers[1].map(el => (
+								<NumberButton buttonStyle="number" text={el} />
+							))}
 						</div>
 						<div className="thirdRow">
-							<NumberButton buttonStyle="number" text="1" />
-							<NumberButton buttonStyle="number" text="2" />
-							<NumberButton buttonStyle="number" text="3" />
+							{numbers[2].map(el => (
+								<NumberButton buttonStyle="number" text={el} />
+							))}
 						</div>
 					</div>
 					<ActionButton buttonStyle="zero" text="0" />
 				</div>
 				<div className="calculatorRight">
-					<MathButton buttonStyle="math" text="÷" />
-					<MathButton buttonStyle="math" text="x" />
-					<MathButton buttonStyle="math" text="-" />
-					<MathButton buttonStyle="math" text="+" />
-					<MathButton buttonStyle="math" text="=" />
+					{mathSymbols.map(el => (
+						<MathButton buttonStyle="math" text={el} />
+					))}
 				</div>
 			</div>
 		</div>
 	);
 };
+
+//Old Code
+{
+	/* <NumberButton buttonStyle="number" text="7" />
+  <NumberButton buttonStyle="number" text="8" />
+  <NumberButton buttonStyle="number" text="9" /> */
+}
+{
+	/* <NumberButton buttonStyle="number" text="4" />
+  <NumberButton buttonStyle="number" text="5" />
+  <NumberButton buttonStyle="number" text="6" /> */
+}
+{
+	/* <NumberButton buttonStyle="number" text="1" />
+  <NumberButton buttonStyle="number" text="2" />
+  <NumberButton buttonStyle="number" text="3" /> */
+}
+{
+	/* <MathButton buttonStyle="math" text="÷" />
+  <MathButton buttonStyle="math" text="×" />
+  <MathButton buttonStyle="math" text="−" />
+  <MathButton buttonStyle="math" text="+" />
+  <MathButton buttonStyle="math" text="=" /> */
+}
 
 export default App;
